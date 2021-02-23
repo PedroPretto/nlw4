6eaaf245510b95703275bfe57906fa31de871f3c
pedro.martins <pedro.martins@khomp.com>
2021-02-23 16:44:11 -0300

    segundo dia de node finalizado

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,12 @@
+import 'reflect-metadata'
 import express, { response } from 'express'
+import './database'
+import { router } from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-  return response.json({message: 'Hello World'})
-})
-
-app.post('/', (request, response)=> {
-  return response.json({message: "Dados salvos com sucesso"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => {
   console.log('Server running')
