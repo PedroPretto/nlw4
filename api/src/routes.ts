280bd0437ffacd4d5b27df819c2e86da14c8ff3e
pedro.martins <pedro.martins@khomp.com>
2021-02-24 14:39:40 -0300

    dia 3 50%

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express'
+import { SurveysControler } from './controllers/SurveysController'
 import { UserController } from './controllers/UserController'
 
 const router = Router()
 
 const userController = new UserController()
-
+const surveysController = new SurveysControler()
 router.post('/users', userController.create)
-
+router.post('/surveys', surveysController.create)
+router.get('/surveys', surveysController.show)
 export {router}
